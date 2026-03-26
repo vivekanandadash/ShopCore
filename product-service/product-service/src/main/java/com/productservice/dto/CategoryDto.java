43634ed45feff011071a7f5b8171ce782cbecdc7
Vivekananda <vivekanandadash245@gmail.com>
2026-03-26 18:45:54 +0530

    Customize CategoryDto As the code was empty or vanished

--- product-service/product-service/src/main/java/com/productservice/dto/CategoryDto.java
+++ product-service/product-service/src/main/java/com/productservice/dto/CategoryDto.java
@@ -1,4 +1,31 @@
 package com.productservice.dto;
 
 public class CategoryDto {
+
+    private Long id;
+    private String name;
+
+    public CategoryDto() {
+    }
+
+    public CategoryDto(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
