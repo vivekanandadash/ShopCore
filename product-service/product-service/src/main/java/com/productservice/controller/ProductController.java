d726a47469f9a7b974934d3d43f66990c8e76124
Vivekananda <vivekanandadash245@gmail.com>
2026-03-26 22:55:52 +0530

    create a ProductController class  for API request response and build find all method & configure properties file

--- product-service/product-service/src/main/java/com/productservice/controller/ProductController.java
+++ product-service/product-service/src/main/java/com/productservice/controller/ProductController.java
@@ -1,10 +1,38 @@
 package com.productservice.controller;
 
+import com.productservice.dto.ApiResponse;
+import com.productservice.dto.CategoryDto;
+import com.productservice.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/product")
 public class ProductController {
 
+    private CategoryService categoryService;
+
+    public ProductController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+    @GetMapping("/list/categories")
+    public ResponseEntity<ApiResponse<List<CategoryDto>>> getCategories(){
+        List<CategoryDto> categoriesDto = categoryService.findAll();
+        ApiResponse<List<CategoryDto>> response = new ApiResponse<>();
+        if (categoriesDto!=null && !categoriesDto.isEmpty()){
+            response.setMessage("All Categories Data fetched");
+            response.setStatus(200);
+            response.setData(categoriesDto);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        response.setMessage("No Categories Data Found");
+        response.setStatus(500);
+        response.setData(null);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
