62aba13ee809793a15ecca2d113e54432ee835de
Vivekananda <vivekanandadash245@gmail.com>
2026-03-30 19:25:08 +0530

    Use the Converted Dto class  from Mapper class to return the ProductsDtos

--- product-service/product-service/src/main/java/com/productservice/service/ProductServiceImpl.java
+++ product-service/product-service/src/main/java/com/productservice/service/ProductServiceImpl.java
@@ -2,9 +2,11 @@ package com.productservice.service;
 
 import com.productservice.dto.ProductDto;
 import com.productservice.entity.Product;
+import com.productservice.mapper.ProductMapper;
 import com.productservice.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -19,6 +21,14 @@ public class ProductServiceImpl implements ProductService{
     @Override
     public List<ProductDto> searchProducts(String keyword) {
         List<Product> products = productRepository.searchProducts(keyword);
-        return List.of();
+       List<ProductDto> productDtos = new ArrayList<>();
+       for (Product product : products){
+//           ProductDto productDto = ProductMapper.convertProductToDto(product);
+//           productDtos.add(productDto);
+
+//           Note:- Both Commented line and bellow are same i, just optimize the code
+           productDtos.add( ProductMapper.convertProductToDto(product));
+       }
+        return productDtos;
     }
 }
