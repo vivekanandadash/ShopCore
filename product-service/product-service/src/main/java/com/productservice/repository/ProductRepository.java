50907dd5e4563e22079ae7e12a1f01e66aa5b81b
Vivekananda <vivekanandadash245@gmail.com>
2026-03-30 18:20:28 +0530

    adding the search jpql query in product repository layer for search feature

--- product-service/product-service/src/main/java/com/productservice/repository/ProductRepository.java
+++ product-service/product-service/src/main/java/com/productservice/repository/ProductRepository.java
@@ -2,6 +2,18 @@ package com.productservice.repository;
 
 import com.productservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("""
+    SELECT DISTINCT p FROM Product p
+    LEFT JOIN p.brands b
+    WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+       OR LOWER(p.subCategory.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+       OR LOWER(b.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+""")
+    List<Product> searchProducts(@Param("keyword") String keyword);
 }
