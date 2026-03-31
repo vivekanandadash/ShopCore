0218903b77b559e4fdd497e43637e984196ac2d2
Vivekananda <vivekanandadash245@gmail.com>
2026-04-01 00:32:51 +0530

    create product controller layer for searching the products

--- product-service/product-service/src/main/java/com/productservice/controller/ProductController.java
+++ product-service/product-service/src/main/java/com/productservice/controller/ProductController.java
@@ -2,11 +2,14 @@ package com.productservice.controller;
 
 import com.productservice.dto.ApiResponse;
 import com.productservice.dto.CategoryDto;
+import com.productservice.dto.ProductDto;
 import com.productservice.service.CategoryService;
+import com.productservice.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,9 +19,11 @@ import java.util.List;
 public class ProductController {
  // Constructor Based Dependency Injection
     private CategoryService categoryService;
+    private ProductService productService;
 
-    public ProductController(CategoryService categoryService) {
+    public ProductController(CategoryService categoryService, ProductService productService) {
         this.categoryService = categoryService;
+        this.productService = productService;
     }
     //Get Mapping For  get the all data
     @GetMapping("/list/categories")
@@ -32,6 +37,23 @@ public class ProductController {
             return new ResponseEntity<>(response, HttpStatus.OK);
         }
         response.setMessage("No Categories Data Found");
+        response.setStatus(500);
+        response.setData(null);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @GetMapping("/list/search")
+    public ResponseEntity<ApiResponse<List<ProductDto>>> searchProducts(
+            @RequestParam String keyword
+    ){
+        List<ProductDto> productsDto = productService.searchProducts(keyword);
+        ApiResponse<List<ProductDto>> response = new ApiResponse<>();
+        if (productsDto!=null && !productsDto.isEmpty()){
+            response.setMessage("All  Data fetched");
+            response.setStatus(200);
+            response.setData(productsDto);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        response.setMessage("No Matching Record Found");
         response.setStatus(500);
         response.setData(null);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
---
+++ product-service/product-service/src/main/java/com/productservice/dto/SubCategoryDto.java
@@ -0,0 +1,4 @@
+package com.productservice.dto;
+
+public class SubCategoryDto {
+}
