f362740b080efe1fc754563a835671f34be47e32
Vivekananda <vivekanandadash245@gmail.com>
2026-04-01 23:53:55 +0530

    Optimize JPQL Sql Query for Optmizing the search feature

--- product-service/product-service/src/main/java/com/productservice/repository/ProductRepository.java
+++ product-service/product-service/src/main/java/com/productservice/repository/ProductRepository.java
@@ -4,16 +4,18 @@ import com.productservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("""
-    SELECT DISTINCT p FROM Product p
-    LEFT JOIN p.brands b
-    WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
-       OR LOWER(p.subCategory.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
-       OR LOWER(b.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
-""")
+        SELECT DISTINCT p FROM Product p
+        LEFT JOIN FETCH p.brands b
+        WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(p.subCategory.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(b.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
+    """)
     List<Product> searchProducts(@Param("keyword") String keyword);
 }
