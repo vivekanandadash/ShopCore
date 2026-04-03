12baa71f86d4d4a0453f336942cc4f2ec1818727
Vivekananda <vivekanandadash245@gmail.com>
2026-04-03 09:20:56 +0530

    implement the service class and create implementation of that methods

---
+++ product-service/product-service/src/main/java/com/productservice/service/S3ServiceImpl.java
@@ -0,0 +1,47 @@
+package com.productservice.service;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class S3ServiceImpl implements S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    public S3ServiceImpl(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        String fileName = UUID.randomUUID() + "-" + originalFilename;
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .build();
+
+        s3Client.putObject(
+                putObjectRequest,
+                RequestBody.fromBytes(file.getBytes())
+        );
+
+        return fileName;
+    }
+
+    public String getFileUrl(String fileName) {
+        return "https://" + bucketName + ".s3.amazonaws.com/" + fileName;
+    }
+}
