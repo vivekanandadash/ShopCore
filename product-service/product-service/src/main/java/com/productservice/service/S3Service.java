94e774f6a6d47d17949a52a9610b68b19cc1e0ec
Vivekananda <vivekanandadash245@gmail.com>
2026-04-03 09:19:01 +0530

    create service layer inteface and add no-body  method inside it

---
+++ product-service/product-service/src/main/java/com/productservice/service/S3Service.java
@@ -0,0 +1,10 @@
+package com.productservice.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface S3Service {
+    public String uploadFile(MultipartFile file)  throws IOException;
+    public String getFileUrl(String fileName);
+}
