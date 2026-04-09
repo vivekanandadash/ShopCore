b59ea6ec758ce9bc1ddb962ec4f1e786113a75bf
Vivekananda <vivekanandadash245@gmail.com>
2026-04-09 20:39:51 +0530

    creating CartService for business logic of this module

--- cart-service/cart-service/src/main/java/com/cart_service/service/CartService.java
+++ cart-service/cart-service/src/main/java/com/cart_service/service/CartService.java
@@ -1,4 +1,39 @@
 package com.cart_service.service;
 
+import com.cart_service.dto.AddToCartRequestDto;
+import com.cart_service.entity.Cart;
+import com.cart_service.repository.CartRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class CartService {
+    private CartRepository cartRepository;
+
+    public CartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+    public Cart addToCart(String uuid, AddToCartRequestDto addToCartRequestDto){
+        Cart cart;
+
+        //Check if uuid is present or not ?
+        if (uuid == null || uuid.isEmpty()) {
+            uuid = UUID.randomUUID().toString();
+            cart = new Cart();
+            cart.setUuid(uuid);
+        }else {
+            Optional<Cart> optionalCart = cartRepository.findByUuid(uuid);
+
+            if (optionalCart.isPresent()) {
+                cart = optionalCart.get();
+            } else {
+                cart = new Cart();
+                cart.setUuid(uuid);
+            }
+        }
+        return null;
+
+    }
 }
