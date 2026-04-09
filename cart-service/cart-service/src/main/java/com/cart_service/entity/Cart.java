1727cd31b0a7bae02e70041cb13dcdc53b4bc25a
Vivekananda <vivekanandadash245@gmail.com>
2026-04-09 15:50:10 +0530

    create entity class of cart & cartitems and adding one to many relationship

--- cart-service/cart-service/src/main/java/com/cart_service/entity/Cart.java
+++ cart-service/cart-service/src/main/java/com/cart_service/entity/Cart.java
@@ -2,6 +2,9 @@ package com.cart_service.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name= "cart")
 public class Cart {
@@ -11,4 +14,41 @@ public class Cart {
 
     @Column(unique = true)
     private String uuid;
+
+    private Long userId;
+
+    @OneToMany(mappedBy = "cart" ,cascade = CascadeType.ALL ,orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
 }
---
+++ cart-service/cart-service/src/main/java/com/cart_service/entity/CartItem.java
@@ -0,0 +1,60 @@
+package com.cart_service.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "cart_item")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private Long brandId;
+    private Integer quantity;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Long brandId) {
+        this.brandId = brandId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+}
