ef4414db6cffdafbf8b49afdae38f0a1049fab32
Vivekananda <vivekanandadash245@gmail.com>
2026-04-09 20:37:32 +0530

    Create AddToCartRequestDto for creating and storing guest user product in cart

--- cart-service/cart-service/src/main/java/com/cart_service/dto/AddToCartRequestDto.java
+++ cart-service/cart-service/src/main/java/com/cart_service/dto/AddToCartRequestDto.java
@@ -1,4 +1,42 @@
 package com.cart_service.dto;
 
+import java.math.BigDecimal;
+
 public class AddToCartRequestDto {
+    private Long productId;
+    private Long brandId;
+    private Integer quantity;
+    private BigDecimal price;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Long brandId) {
+        this.brandId = brandId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
