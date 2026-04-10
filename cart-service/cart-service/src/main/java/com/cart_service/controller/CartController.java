49e72a7d93b74cdfabb3fbb3da1ad278b4a5afcf
Vivekananda <vivekanandadash245@gmail.com>
2026-04-11 01:40:05 +0530

    create controller and create the method  arguments which pass to service layer

--- cart-service/cart-service/src/main/java/com/cart_service/controller/CartController.java
+++ cart-service/cart-service/src/main/java/com/cart_service/controller/CartController.java
@@ -1,4 +1,30 @@
 package com.cart_service.controller;
 
+import com.cart_service.dto.AddToCartRequestDto;
+import com.cart_service.dto.ApiResponse;
+import com.cart_service.entity.Cart;
+import com.cart_service.service.CartService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/cart")
 public class CartController {
+    private CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+    @PostMapping("/add")
+   public ResponseEntity<ApiResponse<String>> addToCart(
+            @RequestHeader(value = "X-CART-ID", required = false) String uuid,
+            @RequestBody AddToCartRequestDto addToCartRequestDto
+    ){
+        Cart cart = cartService.addToCart(uuid, addToCartRequestDto);
+        //till above i created logic from tomorrow morning I will  proceed further
+        return null;
+    }
+
 }
