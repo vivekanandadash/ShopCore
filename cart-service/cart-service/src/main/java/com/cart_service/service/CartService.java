3c601ec04a260a9aa55352804f7e1b7a51bad4c4
Vivekananda <vivekanandadash245@gmail.com>
2026-04-11 00:56:51 +0530

    Check wheather the product is exists in the product cart or not if exist then increase the quantity aand if not then add new product

--- cart-service/cart-service/src/main/java/com/cart_service/service/CartService.java
+++ cart-service/cart-service/src/main/java/com/cart_service/service/CartService.java
@@ -2,6 +2,7 @@ package com.cart_service.service;
 
 import com.cart_service.dto.AddToCartRequestDto;
 import com.cart_service.entity.Cart;
+import com.cart_service.entity.CartItem;
 import com.cart_service.repository.CartRepository;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +33,22 @@ public class CartService {
                 cart = new Cart();
                 cart.setUuid(uuid);
             }
+        }
+        // 2 .Check if product is already exists
+        Optional<CartItem> existingCartItems = cart.getItems().stream()
+                .filter(item -> item.getProductId().equals(addToCartRequestDto.getProductId()))
+                .findFirst();
+        if (existingCartItems.isPresent()){
+            CartItem cartItem = existingCartItems.get();
+            cartItem.setQuantity(cartItem.getQuantity() + addToCartRequestDto.getQuantity());
+        } else {
+            CartItem cartItem = new CartItem();
+            cartItem.setProductId(addToCartRequestDto.getProductId());
+            cartItem.setBrandId(addToCartRequestDto.getBrandId());
+            cartItem.setQuantity(addToCartRequestDto.getQuantity());
+            cartItem.setPrice(addToCartRequestDto.getPrice());
+
+            cart.getItems().add(cartItem);
         }
         return null;
 
