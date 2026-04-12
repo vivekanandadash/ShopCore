ceff54c5cfe1b01deb4ae7aba77c8dd7c9052236
Vivekananda <vivekanandadash245@gmail.com>
2026-04-12 10:19:48 +0530

    convert cart and cartItem entity class  to Dto class so that we can use that in service layer

---
+++ cart-service/cart-service/src/main/java/com/cart_service/dto/CartDto.java
@@ -0,0 +1,42 @@
+package com.cart_service.dto;
+
+import java.util.List;
+
+public class CartDto {
+    private Long id;
+    private String uuid;
+    private Long userId;
+    private List<CartItemDto> items;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<CartItemDto> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItemDto> items) {
+        this.items = items;
+    }
+}
---
+++ cart-service/cart-service/src/main/java/com/cart_service/dto/CartItemDto.java
@@ -0,0 +1,51 @@
+package com.cart_service.dto;
+
+import java.math.BigDecimal;
+
+public class CartItemDto {
+    private Long id;
+    private Long productId;
+    private Long brandId;
+    private Integer quantity;
+    private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Long brandId) {
+        this.brandId = brandId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
