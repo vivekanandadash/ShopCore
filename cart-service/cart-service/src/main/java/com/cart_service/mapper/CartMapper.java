e800907ff3fa83d5e4a33dba6a06ee5dba55efa6
Vivekananda <vivekanandadash245@gmail.com>
2026-04-12 10:44:52 +0530

    Create CartMapper class to convert Cart & CartItem entity to dto class so that we can use in service and controller layer insted of using direct entity class

--- cart-service/cart-service/src/main/java/com/cart_service/mapper/CartMapper.java
+++ cart-service/cart-service/src/main/java/com/cart_service/mapper/CartMapper.java
@@ -1,4 +1,38 @@
 package com.cart_service.mapper;
 
+import com.cart_service.dto.CartDto;
+import com.cart_service.dto.CartItemDto;
+import com.cart_service.entity.Cart;
+import com.cart_service.entity.CartItem;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartMapper {
+
+    public static CartDto convertCartToDto(Cart cart) {
+        CartDto cartDto = new CartDto();
+        cartDto.setId(cart.getId());
+        cartDto.setUuid(cart.getUuid());
+        cartDto.setUserId(cart.getUserId());
+
+        // Normal for loop instead of stream
+        List<CartItemDto> itemDtos = new ArrayList<>();
+        for (CartItem cartItem : cart.getItems()) {
+            CartItemDto cartItemDto = convertCartItemToDto(cartItem);
+            itemDtos.add(cartItemDto);
+        }
+
+        cartDto.setItems(itemDtos);
+        return cartDto;
+    }
+
+    public static CartItemDto convertCartItemToDto(CartItem cartItem) {
+        CartItemDto cartItemDto = new CartItemDto();
+        cartItemDto.setId(cartItem.getId());
+        cartItemDto.setProductId(cartItem.getProductId());
+        cartItemDto.setBrandId(cartItem.getBrandId());
+        cartItemDto.setQuantity(cartItem.getQuantity());
+        cartItemDto.setPrice(cartItem.getPrice());
+        return cartItemDto;
+    }
 }
