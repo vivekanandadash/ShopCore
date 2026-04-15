7cf41b58a6bffa4c334dd7cbd9a76acb0d3d412f
Vivekananda <vivekanandadash245@gmail.com>
2026-04-15 23:55:39 +0530

    add feign client for interservice communication

---
+++ order-service/src/main/java/com/order_service/client/CartFeignClient.java
@@ -0,0 +1,17 @@
+package com.order_service.client;
+
+import com.order_service.dto.CartDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "cart-service" , url = "http://localhost:8083")
+public interface CartFeignClient {
+    @GetMapping("/api/v1/card/{uuid}")
+    CartDto getCart(@PathVariable String uuid);
+
+    @DeleteMapping("/api/v1/cart/{uuid}/clear")
+    void clearCart(@PathVariable String uuid);
+
+}
