0ece5964a45620b05da0cd80ed51bf16a875db57
Vivekananda <vivekanandadash245@gmail.com>
2026-04-16 16:55:21 +0530

    Creating a CartFeignClient interface which has two incomplete methods for get and clear the cart which will implementing in cartService module . this interface main purpose to get the cart data from cartservice through feign client for interservice communication

--- order-service/src/main/java/com/order_service/client/CartFeignClient.java
+++ order-service/src/main/java/com/order_service/client/CartFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "cart-service" , url = "http://localhost:8083")
 public interface CartFeignClient {
-    @GetMapping("/api/v1/card/{uuid}")
+    @GetMapping("/api/v1/cart/{uuid}")
     CartDto getCart(@PathVariable String uuid);
 
     @DeleteMapping("/api/v1/cart/{uuid}/clear")
