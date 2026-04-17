8d3ab46a8e57134998153b097fe008776f7a30a0
Vivekananda <vivekanandadash245@gmail.com>
2026-04-17 23:52:13 +0530

    creating api response for same format api response to client

--- order-service/src/main/java/com/order_service/dto/ApiResponse.java
+++ order-service/src/main/java/com/order_service/dto/ApiResponse.java
@@ -1,4 +1,31 @@
 package com.order_service.dto;
 
-public class ApiResponse {
+public class ApiResponse<T> {
+    private String message;
+    private Integer status;
+    private T data;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
