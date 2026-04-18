bbf96b3c60509cbb086259cf1046d54a16f33fcd
Vivekananda <vivekanandadash245@gmail.com>
2026-04-18 22:24:24 +0530

    creating dto layer for both Order and orderItem Entity

--- order-service/src/main/java/com/order_service/dto/OrderDto.java
+++ order-service/src/main/java/com/order_service/dto/OrderDto.java
@@ -1,4 +1,78 @@
 package com.order_service.dto;
 
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class OrderDto {
+
+    private Long id;
+
+    private String cartUuid;
+    private Long userId;
+
+    private BigDecimal totalAmount;
+
+    private String status;
+
+    private LocalDateTime createdAt;
+
+    private List<OrderItemDto>items;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCartUuid() {
+        return cartUuid;
+    }
+
+    public void setCartUuid(String cartUuid) {
+        this.cartUuid = cartUuid;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<OrderItemDto> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItemDto> items) {
+        this.items = items;
+    }
 }
---
+++ order-service/src/main/java/com/order_service/mapper/OrderMapper.java
@@ -0,0 +1,4 @@
+package com.order_service.mapper;
+
+public class OrderMapper {
+}
