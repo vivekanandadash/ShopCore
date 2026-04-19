ec1df16386c4f16d471be8f5bd9c10c6592ca0ef
Vivekananda <vivekanandadash245@gmail.com>
2026-04-19 18:34:21 +0530

    Set up the stipe  configuration file for payment service

--- payment-service/src/main/java/com/payment_service/config/StripeConfig.java
+++ payment-service/src/main/java/com/payment_service/config/StripeConfig.java
@@ -1,4 +1,17 @@
 package com.payment_service.config;
 
+import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class StripeConfig {
+    @Value("${stripe.secret.key}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
 }
