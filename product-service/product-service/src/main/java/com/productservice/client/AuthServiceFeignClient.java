b3748df5e2be311807d16063331a731efd9f0f48
Vivekananda <vivekanandadash245@gmail.com>
2026-04-25 15:07:12 +0530

    Creating AuthServiceFeignClient to communicate with auth service for getting the userCredential

--- product-service/product-service/src/main/java/com/productservice/client/AuthServiceFeignClient.java
+++ product-service/product-service/src/main/java/com/productservice/client/AuthServiceFeignClient.java
@@ -1,4 +1,15 @@
 package com.productservice.client;
 
+import com.productservice.dto.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@FeignClient(name = "AUTHSERVICEAPP")
 public interface AuthServiceFeignClient {
+
+    @GetMapping("/api/v1/auth/get-user")
+    User getUserByUsername(@RequestParam("username") String username, @RequestHeader("Authorization") String token);
 }
