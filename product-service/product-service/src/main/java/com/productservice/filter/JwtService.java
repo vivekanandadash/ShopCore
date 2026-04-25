5e6386bf34424a6ea91e08120e672655b95098d2
Vivekananda <vivekanandadash245@gmail.com>
2026-04-25 15:14:28 +0530

    validating usertoken

--- product-service/product-service/src/main/java/com/productservice/filter/JwtService.java
+++ product-service/product-service/src/main/java/com/productservice/filter/JwtService.java
@@ -1,4 +1,27 @@
 package com.productservice.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtService {
+    private static final String SECRET_KEY = "secret12345";
+    private static final long EXPIRATION_TIME = 86400000; // 1 day
+
+//    public String generateToken(String username, String role) {
+//        return JWT.create()
+//            .withSubject(username)
+//            .withClaim("role", role)
+//            .withIssuedAt(new Date())
+//            .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+//            .sign(Algorithm.HMAC256(SECRET_KEY));
+//    }
+
+    public String validateTokenAndRetrieveSubject(String token) {
+        return JWT.require(Algorithm.HMAC256(SECRET_KEY))
+                .build()
+                .verify(token)
+                .getSubject();
+    }
 }
