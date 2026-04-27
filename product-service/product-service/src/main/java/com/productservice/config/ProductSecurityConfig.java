6fc3583571d136841b95d2b39d6870e81d1e2e56
Vivekananda <vivekanandadash245@gmail.com>
2026-04-27 23:27:36 +0530

    create Config file for add the bean into spring ioc so that when project run spiring ioc understand that which are the is open and which are protected

--- product-service/product-service/src/main/java/com/productservice/config/ProductSecurityConfig.java
+++ product-service/product-service/src/main/java/com/productservice/config/ProductSecurityConfig.java
@@ -1,4 +1,31 @@
 package com.productservice.config;
 
+import com.productservice.filter.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class ProductSecurityConfig {
+
+    @Autowired
+    private JwtFilter filter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/api/v1/product/list/**").permitAll()
+                    .requestMatchers("/api/v1/product/add").hasRole("STORE")
+                            .anyRequest().authenticated();
+                })
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
 }
